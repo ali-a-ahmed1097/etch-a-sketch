bc7ff3be991c0730655146dc27b6afae5252d9f1
Ali <ali-a-ahmed1097@hotmail.com>
2022-10-12 16:40:33 -0400

    Can draw on the board properly. Using clicking and dragging. One slight problem is if when the border is clicked, a move is attempted and therefore mousedown event is preserved.

--- etch.js
+++ etch.js
@@ -1,6 +1,30 @@
 const GRID_WIDTH = 16;
 const GRID_HEIGHT = 16;
 
+function setSquareColor(e) {
+    e.target.setAttribute('style', 'background-color: black;')
+}
+
+function gridDraw(gContainer){
+    const squares = document.querySelectorAll('.grid-square');
+
+    squares.forEach(function (sqr) {
+        sqr.addEventListener('mousedown', setSquareColor);
+    });
+
+    gContainer.addEventListener('mousedown', function () {
+        squares.forEach(function (sqr) {
+            sqr.addEventListener('mouseover', setSquareColor);
+        })
+    });
+
+    gContainer.addEventListener('mouseup', function () {
+        squares.forEach(function (sqr) {
+            sqr.removeEventListener('mouseover', setSquareColor);
+        });
+    });
+}
+
 function createGrid() {
     const container = document.querySelector('.grid-container');
     for (let i = 0; i < GRID_WIDTH*GRID_HEIGHT; i++)
@@ -9,6 +33,7 @@ function createGrid() {
         sqr.classList.add('grid-square');
         container.appendChild(sqr);
     }
+    gridDraw(container);
 }
 
 createGrid();
